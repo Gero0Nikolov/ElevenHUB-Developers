643d89035c37261981fa127a98f68e024abf2f3a
Gero0Nikolov <vtm.sunrise@gmail.com>
2017-11-19 00:07:27 +0200

    ElevenHUB - Developers initial

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * 11hub Developers Theme Customizer
+ *
+ * @package 11hub_Developers
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function elhub_dev_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'elhub_dev_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'elhub_dev_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'elhub_dev_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function elhub_dev_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function elhub_dev_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function elhub_dev_customize_preview_js() {
+	wp_enqueue_script( 'elhub_dev-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'elhub_dev_customize_preview_js' );
